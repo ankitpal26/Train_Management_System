f19bdeef0bf3d0c4cd43019fffd49853266b8af0
Ankit Pal <anpal@altimetrik.com>
2023-12-18 18:24:25 +0530

    Service registry server created

---
+++ ServiceRegistry/ServiceRegistry/src/test/java/com/altimetrik/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
