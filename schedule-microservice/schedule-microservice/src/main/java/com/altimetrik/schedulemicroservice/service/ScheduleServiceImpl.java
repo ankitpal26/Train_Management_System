f890957b1da1d8a30a22070b2a33a2ff308ab234
My Name <ankitpal28090016@gmail.com>
2023-12-19 19:53:11 +0530

    ScheduleImpl service updated

--- schedule-microservice/schedule-microservice/src/main/java/com/altimetrik/schedulemicroservice/service/ScheduleServiceImpl.java
+++ schedule-microservice/schedule-microservice/src/main/java/com/altimetrik/schedulemicroservice/service/ScheduleServiceImpl.java
@@ -7,7 +7,6 @@ import com.altimetrik.schedulemicroservice.model.Train;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
