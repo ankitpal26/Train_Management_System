8dc8f4885fdb769ce48d628867384a04422823c9
My Name <ankitpal28090016@gmail.com>
2023-12-21 13:18:53 +0530

    app config updated

--- schedule-microservice/schedule-microservice/src/main/java/com/altimetrik/schedulemicroservice/config/AppConfig.java
+++ schedule-microservice/schedule-microservice/src/main/java/com/altimetrik/schedulemicroservice/config/AppConfig.java
@@ -3,7 +3,6 @@ package com.altimetrik.schedulemicroservice.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
