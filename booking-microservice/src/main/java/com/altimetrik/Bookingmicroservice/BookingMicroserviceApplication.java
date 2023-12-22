a8208345f135fbf1e0f75c50089173f19a17d60f
My Name <ankitpal28090016@gmail.com>
2023-12-22 16:57:31 +0530

    inital commit for booking service

---
+++ booking-microservice/src/main/java/com/altimetrik/Bookingmicroservice/BookingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik.Bookingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingMicroserviceApplication.class, args);
+	}
+
+}
